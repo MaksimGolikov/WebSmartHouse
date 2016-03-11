47760abaa1d6c681c4100ecb42bac52537863f86
Maksimus95 <golikov.mo@gmail.com>
2016-03-11 10:47:26 +0200

    https://github.com/Maksimus95/MVC.git

---
+++ MVC/MvcSmartHouse/MvcSmartHouse/Controllers/APIController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MvcSmartHouse.Controllers
+{
+    public class APIController : ApiController
+    {
+       [HttpPost]
+        public IDictionary<int, Models.Devices.Device> Switch([FromBody]  IDictionary<int, Models.Devices.Device> device)
+        {              
+                   return device;
+                 
+        }
+    }
+}
